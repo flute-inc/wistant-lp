99036108927874598a911bc84868e5d843dd2269
relations-mashiko <relations.mashiko@gmail.com>
2019-07-04 11:43:05 +0900

    library indexの箱を用意

---
+++ src/pages/library.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Seo from '../components/atoms/Seo';
+import Posts from '../components/pages/Root/Posts';
+import Layout from '../components/templates/Layout';
+
+const LibraryIndex = ({ location, data }) => (
+  <Layout location={location} title={data.site.siteMetadata.title}>
+    <Seo title="Index" description="なければ作ればいいじゃない" />
+    <div>Heading</div>
+    <div>Finder</div>
+    <div>
+      <div>Label</div>
+      <Posts posts={data.allMarkdownRemark.edges} />
+    </div>
+    <div>
+      <div>Label</div>
+      <div>Post</div>
+      <div>Post</div>
+      <div>Post</div>
+    </div>
+  </Layout>
+);
+
+export default LibraryIndex;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "YYYY/M/D")
+            title
+            description
+            tags
+            hero {
+              childImageSharp {
+                fixed(width: 128, height: 128) {
+                  ...GatsbyImageSharpFixed
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
